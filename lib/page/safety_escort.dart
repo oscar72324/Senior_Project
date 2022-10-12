2dc1e5c6e7438fe4a96d3933702408596a6788d8
oscar72324 <oscar.ibarra.194@my.csun.edu>
2022-10-12 11:03:10 -0700

    commit

--- lib/page/safety_escort.dart
+++ lib/page/safety_escort.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class SafetyEscort extends StatelessWidget {
+  const SafetyEscort({super.key});
+
   @override
   Widget build(BuildContext context) => Scaffold(
         appBar: AppBar(
